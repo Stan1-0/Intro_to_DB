584b2a32f2c6f706133ef8e195d7f3146c29cdaa
Stan1-0 <stankofb@gmail.com>
2025-10-14 18:37:41 +0000

    "Replaced SQL query with SHOW TABLES statement in task_3.sql"

--- task_3.sql
+++ task_3.sql
@@ -1,9 +1,1 @@
-SELECT 
-    table_schema AS "Database Name",
-    table_name AS "Table Name"
-FROM 
-    information_schema.tables
-WHERE 
-    table_schema = LOWER(?)
-ORDER BY 
-    table_schema, table_name;
+SHOW TABLES;
